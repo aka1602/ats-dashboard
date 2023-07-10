88e859094a3a5e7024f263c93ac6b5aaa6794f56
Akash <akash.greenwork1@gmail.com>
2023-07-10 22:48:40 +0530

    feat: database setup

--- src/index.ts
+++ src/index.ts
@@ -3,58 +3,23 @@ import http from 'http';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { MongoClient, ServerApiVersion } from 'mongodb';
+dotenv.config();
+const mongoString: string = process.env.DATABASE_URL || '';
 
-const uri =
-	'mongodb+srv://akashgreenwork:atsDashboard123@ats-cluster.jj8g568.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoString);
+const database = mongoose.connection;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-	serverApi: {
-		version: ServerApiVersion.v1,
-		strict: true,
-		deprecationErrors: true,
-	},
+database.on('error', (error) => {
+	console.log(error);
 });
 
-async function run() {
-	try {
-		// Connect the client to the server	(optional starting in v4.7)
-		await client.connect();
-		// Send a ping to confirm a successful connection
-		await client.db('admin').command({ ping: 1 });
-		console.log(
-			'Pinged your deployment. You successfully connected to MongoDB!'
-		);
-	} finally {
-		// Ensures that the client will close when you finish/error
-		await client.close();
-	}
-}
-run().catch(console.dir);
+database.once('connected', () => {
+	console.log('Database Connected');
+});
+const app = express();
 
-// const app = express();
-// dotenv.config();
+app.use(express.json());
 
-// app.use(express.json());
-
-// app.get('/', (req: express.Request, res: express.Response) => {
-// 	return res
-// 		.status(200)
-// 		.send({
-// 			message: 'Jo',
-// 		})
-// 		.end();
-// });
-
-// const server = http.createServer(app);
-
-// server.listen(8000, () => {
-// 	console.log('http://localhost:8000/');
-// });
-
-// const MONGO_URL = `${process.env.DATABASE_URL}`;
-// mongoose.Promise = Promise;
-// mongoose.connect(MONGO_URL);
-// mongoose.connection.on('error', (error: Error) => console.log(error));
-
-// // app.use("/", router());
+app.listen(3000, () => {
+	console.log(`Server Started at ${3000}`);
+});
