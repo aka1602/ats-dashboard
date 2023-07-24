6317c5bfb8a4a3eb97a5e7377bbe844f13874a00
divya-prabhakar29 <prabhakardivya02@gmail.com>
2023-07-24 21:54:58 +0530

    Master

---
+++ src/sender.ts
@@ -0,0 +1,35 @@
+import  * as  nodemailer from "nodemailer";
+
+import    * as Mail from "nodemailer/lib/mailer";
+
+
+
+async function main (){
+  const transporter = nodemailer.createTransport({
+    host :"gmail",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+      user: "prabhakardivya02@gmail.com",
+      pass: "dlepyaavngyxubgz",
+    },
+    logger: true
+  });
+
+  
+  const info = await transporter.sendMail({
+    from: "prabhakardivya02@gmail.com",
+    to: "divya29prabhakar@gmail.com",
+    subject: "Hello from node",
+    text: "Hello world?",
+    html: {path :"D:/ATS_backend/ats-dashboard/src/Amp-index.html"  },
+  
+      
+    //"<strong>Hello world?</strong>"
+    
+  });
+
+  console.log("Message sent: %s", info.response);
+
+}
